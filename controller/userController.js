e5810be3401aacbd843f7d5fbb95f8f5c5701310
marielprado <mariel.prado24@gmail.com>
2022-07-04 20:04:11 -0300

    registro de usuario

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,34 @@
+const db = require("../data/db-users");
+const users = db.getAll();
 
 const userController ={
+    // Registro de usuario
     register: (req, res)=>{
         res.render('usuario/register')
+    },
+
+    store: (req, res) => {
+        const newUser = req.body;
+        if (users.length) {
+            newUser.id = users[users.length - 1].id + 1;            
+        } else {
+            newUser.id = 1;
+        }
+        
+        //CAMBIAR CAMPOS
+        newUser.first_name = req.body.name;
+        newUser.price= req.body.price;
+        newUser.category=req.body.category;
+        newUser.description=req.body.description;
+        if (req.file){
+            newUser.image=req.file.filename; /*******************/
+        }else{
+            newUser.image="faltaimg.jpg"
+        }       
+
+        users.push(newUser);
+        db.saveAll(users);
+        res.redirect("/");
     },
     
     login: (req, res)=>{
--- rutas/rutaUser.js
+++ rutas/rutaUser.js
@@ -1,9 +1,39 @@
 const express = require('express');
 
 const router= express.Router();
+const multer = require("multer");
+const path = require('path');
 const userController= require("../controller/userController");
+ 
+/***************para carga de archivo**************************/
+let multerDiskStorage= multer.diskStorage({
+    destination:(req, file, cb)=>{        
+        cb(null, path.join(__dirname, '../public/imagenes'));
+    },
+    filename: (req,file,cb)=>{
+        const newImagen= Date.now() + path.extname(file.originalname);
+        cb(null, newImagen);
+    }
+});
 
-router.get("/register",userController.register);
+const fileUpload= multer({storage: multerDiskStorage});
+/******************************************************************/
+
+/*registrar un usuario*/ 
+router.get('/register', userController.register); 
+//router.post('/guardar', productoController.store); 
+router.post('/guardar', fileUpload.single('image'),  userController.store);
+
 router.get("/login",userController.login);
+
+/*obtener un solo usuario*/
+//router.get('/:id', userController.detail);
+ 
+/*editar, actualizar y eliminar un usuario*/ 
+//router.get("/edit/:id", userController.edit);
+//router.put("/update/:id", fileUpload.single('image'), userController.update);
+//router.delete("/delete/:id", userController.destroy);
+
+
 
 module.exports=router;
