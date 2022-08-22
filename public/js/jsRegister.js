a708cfb465a9aef3496de13055988033ee212ea6
marielprado <mariel.prado24@gmail.com>
2022-08-22 18:00:29 -0300

    validaciones front

--- public/js/jsRegister.js
+++ public/js/jsRegister.js
@@ -4,62 +4,69 @@ window.addEventListener("load", function () {
   
 
   formulario.addEventListener("submit", function (event) {
-    let campoNombre = document.querySelector('#name');
+    event.preventDefault();
+    let errores = [];
+    /*let campoNombre = document.querySelector('#name');
     let campoNombreUsuario = document.querySelector('#user');
     let campoEmail = document.querySelector('#email');
     let campoContraseña = document.querySelector('#password');
-    let campoImagen = document.querySelector('#image');
+    let campoImagen = document.querySelector('#image');*/
 
-    event.preventDefault();
-    let errores = [];
-    //validaciones
-
-//validacion extencion imagen falta
-    
+    //VALIDACIONES    
 
     //validacion nombre
-    if (campoNombre.value == "") {
+    if (formulario.name.value == "") {
       errores.push('El nombre no puede estar vacio');
-    }
-    if (campoNombre.value.length <= 2) {
+      formulario.querySelector(".errorname").innerHTML = "El nombre no puede estar vacío"
+    } else if (formulario.name.value.length < 2) {
       errores.push('El nombre debe tener al menos dos caracteres');
-    }
+      formulario.querySelector(".errorname").innerHTML = "El nombre debe tener al menos dos caracteres"
+    } else {
+    formulario.querySelector(".errorname").innerHTML = ""
+    };
 
     //validacion nombre usuario
-    if (campoNombreUsuario.value == "") {
+    if (formulario.user.value == "") {
       errores.push('El nombre de usuario no puede estar vacio');
-    }
-    if (campoNombreUsuario.value.length <= 2) {
-      errores.push('El nombre debe tener al menos dos caracteres');
+      formulario.querySelector(".erroruser").innerHTML = "El nombre de usuario no puede estar vacío"
+    } else if (formulario.user.value.length <= 2) {
+      errores.push('El nombre de usuario debe tener al menos dos caracteres');
+      formulario.querySelector(".erroruser").innerHTML = "El nombre debe tener al menos dos caracteres"
+    } else {
+      formulario.querySelector(".erroruser").innerHTML = ""
     }
 
     //validacion mail (cuando pongo un mail no valido no aparecen los errores)
-    
-    if (campoEmail.value == "") {
-      errores.push('El mail no puede estar vacio');
-    }
-    if (campoEmail.value.isEmail == false) {
-      errores.push('Debes ingresar un mail valido');
-    }
-        
+   if (formulario.email.value == "") {
+      errores.push('El email no puede estar vacio');
+      formulario.querySelector(".erroremail").innerHTML = "El email no puede estar vacio"
+    } else if (formulario.email.value.isEmail == false) {
+      errores.push('Debes ingresar un email valido');
+      formulario.querySelector(".erroremail").innerHTML = "Debes ingresar un email válido"
+    } else {
+      formulario.querySelector(".erroremail").innerHTML = ""
+    }    
 
      //validacion contraseña
-     if (campoContraseña.value == "") {
-      errores.push('La contraseña no puede estar vacia');
-    }
-    if (campoContraseña.value.length < 8) {
+   if (formulario.password.value == "") {
+      errores.push('Debes ingresar una contraseña');
+      formulario.querySelector(".errorpassword").innerHTML = "Debes ingresar una contraseña"
+    } else if (formulario.password.value.length < 8) {
       errores.push('La contraseña debe tener al menos 8 caracteres');
+      formulario.querySelector(".errorpassword").innerHTML = "La contraseña debe tener al menos 8 caracteres"
+    } else {
+      formulario.querySelector(".errorpassword").innerHTML = ""
     }
 
-     //si no fallaron las validaciones 
+     // SI NO HAY ERRORES 
     if (errores.length == 0) {
       formulario.submit();
-    } else {
-      let ulErrores = document.querySelector("div.errores ul");
+    } else { console.log(errores)
+      /*let ulErrores = document.querySelector("div.errores ul");
       for (let i = 0; i < errores.length; i++){
         ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
       }
     }
 	
   })
-})
+})*/ }})})
