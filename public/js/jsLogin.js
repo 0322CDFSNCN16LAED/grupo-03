d5dc66adfda19da72590d4c1468e68048aa135da
marielprado <mariel.prado24@gmail.com>
2022-09-06 20:44:18 -0300

    se arregla register/login frontend

--- public/js/jsLogin.js
+++ public/js/jsLogin.js
@@ -6,15 +6,15 @@ window.addEventListener("load", function () {
   
   formulario.addEventListener("submit", function (event) {
     event.preventDefault();
-   // let errores = [];
+    let errores = [];
       
     //VALIDACION EMAIL
     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (formulario.email.value == "") {
-      //errores.push('El email no puede estar vacío');
+      errores.push('El email no puede estar vacío');
       formulario.querySelector(".erroremail").innerHTML = "El email no puede estar vacío"
     } else if (!filter.test(formulario.email.value)) {
-      //errores.push('Debes ingresar un email valido');
+      errores.push('Debes ingresar un email valido');
       formulario.querySelector(".erroremail").innerHTML = "Debes ingresar un email válido"
     } else {
       formulario.querySelector(".erroremail").innerHTML = ""
@@ -22,7 +22,7 @@ window.addEventListener("load", function () {
       
     //VALIDACION CONTRASEÑA
     if (formulario.password.value == "") {
-      //errores.push('Debes ingresar una contraseña');
+      errores.push('Debes ingresar una contraseña');
       formulario.querySelector(".errorpassword").innerHTML = "Debes ingresar una contraseña"
     } else {
       formulario.querySelector(".errorpassword").innerHTML = ""
@@ -31,7 +31,7 @@ window.addEventListener("load", function () {
        //si no fallaron las validaciones 
       if (errores.length == 0) {
         formulario.submit();
-      } // else {console.log(errores)}      
+      } else {console.log(errores)}      
     })
   })
   
--- public/js/jsRegister.js
+++ public/js/jsRegister.js
@@ -5,14 +5,16 @@ window.addEventListener("load", function () {
 
   formulario.addEventListener("submit", function (event) {
     event.preventDefault();
+    let errores = []
 
     //VALIDACIONES
     // validacion imagen
     var filename = formulario.image.value
     var extension = filename.substring(filename.lastIndexOf(".")+1).toLowerCase()
-    if (extension=="jpg"|| extension=="jpeg" || extension=="png" || extension=="gif" || filename == ""){
+    if (extension=="jpg"|| extension=="jpeg" || extension=="png" || extension=="gif" || filename == ""){ 
     formulario.querySelector(".errorimage").innerHTML = ""
    } else {
+    errores.push('error en la imagen'); 
     formulario.querySelector(".errorimage").innerHTML = "No se admiten archivos de extensión " + "'" + extension.toUpperCase() + "'." + " Archivos permitidos: JPG, JPEG, PNG o GIF."
     }
       
@@ -27,8 +29,10 @@ window.addEventListener("load", function () {
 
     //validacion nombre
     if (formulario.name.value == "") {
+      errores.push('error: nombre vacío');  
       formulario.querySelector(".errorname").innerHTML = "El nombre no puede estar vacío"
-    } else if (formulario.name.value.length < 2) {
+    } else if (formulario.name.value.length <= 2) {
+      errores.push('error: nombre menor a 2 dígitos');  
       formulario.querySelector(".errorname").innerHTML = "El nombre debe tener al menos dos caracteres"
     } else {
       formulario.querySelector(".errorname").innerHTML = ""
@@ -36,8 +40,10 @@ window.addEventListener("load", function () {
 
     //validacion nombre usuario
     if (formulario.user.value == "") {
+      errores.push('error: usuario vacío');  
       formulario.querySelector(".erroruser").innerHTML = "El nombre de usuario no puede estar vacío"
     } else if (formulario.user.value.length <= 2) {
+      errores.push('error: usuario menor a 2 dígitos'); 
       formulario.querySelector(".erroruser").innerHTML = "El nombre debe tener al menos dos caracteres"
     } else {
       formulario.querySelector(".erroruser").innerHTML = ""
@@ -46,9 +52,11 @@ window.addEventListener("load", function () {
     //validacion mail 
     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (formulario.email.value == "") {
+      errores.push('error: mail vacío '); 
       formulario.querySelector(".erroremail").innerHTML = "El email no puede estar vacio"
     } //else if (formulario.email.body.isEmail == false) {
     else if (!filter.test(formulario.email.value)) {
+      errores.push('error: mail no válido '); 
       formulario.querySelector(".erroremail").innerHTML = "Debes ingresar un email válido"
     } else {
       formulario.querySelector(".erroremail").innerHTML = ""
@@ -56,8 +64,10 @@ window.addEventListener("load", function () {
 
     //validacion contraseña
     if (formulario.password.value == "") {
+      errores.push('error: contraseña vacía');
       formulario.querySelector(".errorpassword").innerHTML = "Debes ingresar una contraseña"
     } else if (formulario.password.value.length < 8) {
+      errores.push('error: contraseña menor a 8 dígitos');
       formulario.querySelector(".errorpassword").innerHTML = "La contraseña debe tener al menos 8 caracteres"
     } else {
       formulario.querySelector(".errorpassword").innerHTML = ""
@@ -65,6 +75,7 @@ window.addEventListener("load", function () {
 
     //validación confirmar contraseña
     if (formulario.confirmar.value != formulario.password.value) {
+      errores.push('error: contraseñas no coinciden');
       formulario.querySelector(".errorconfirmar").innerHTML = "Las contraseñas no coinciden"
     } else {
       formulario.querySelector(".errorconfirmar").innerHTML = ""
@@ -73,7 +84,7 @@ window.addEventListener("load", function () {
     // SI NO HAY ERRORES 
     if (errores.length == 0) {
       formulario.submit();
-    }
+    } else {console.log(errores)}
   
   })
 })
