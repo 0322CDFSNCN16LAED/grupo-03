97f4bb3ae0ed32461b2adecbca52fb5a6943c1fc
marielprado <mariel.prado24@gmail.com>
2022-09-06 21:08:47 -0300

    se arregla add/edit productos frontend

--- public/js/jsProductAdd.js
+++ public/js/jsProductAdd.js
@@ -4,12 +4,15 @@ window.addEventListener("load", function () {
     
   
   formulario.addEventListener("submit", function (event) {
-    event.preventDefault();      
+    event.preventDefault();  
+    let errores = [];    
     
     //validacion nombre
     if (formulario.name.value == "") {
+      errores.push('error: nombre vacío'); 
       formulario.querySelector(".errorname").innerHTML = "El nombre no puede estar vacío"
-    } else if (formulario.name.value.length < 5) {
+    } else if (formulario.name.value.length <= 5) {
+      errores.push('error: nombre menor a 5 dígitos'); 
       formulario.querySelector(".errorname").innerHTML = "El nombre debe tener al menos cinco caracteres"
     } else {
       formulario.querySelector(".errorname").innerHTML = ""
@@ -18,8 +21,10 @@ window.addEventListener("load", function () {
   
     //validacion precio
     if (formulario.price.value == "") {
+      errores.push('precio vacío'); 
       formulario.querySelector(".errorprice").innerHTML = "El precio no puede estar vacío"
     } else if (formulario.price.value <= 0) {
+      errores.push('precio menor a 0'); 
       formulario.querySelector(".errorprice").innerHTML = "El precio debe ser mayor a cero"
     } else {
       formulario.querySelector(".errorprice").innerHTML = ""
@@ -31,14 +36,17 @@ window.addEventListener("load", function () {
     if (extension=="jpg"|| extension=="jpeg" || extension=="png" || extension=="gif" || filename == ""){
     formulario.querySelector(".errorimage").innerHTML = ""
    } else {
+    errores.push('error en la imagen'); 
     formulario.querySelector(".errorimage").innerHTML = "No se admiten archivos de extensión " + "'" + extension.toUpperCase() + "'." + " Archivos permitidos: JPG, JPEG, PNG o GIF."
     }
     
     //validacion Descripcion 
     if (formulario.description.value == "") {
+      errores.push('descripción vacía'); 
       formulario.querySelector(".errordescription").innerHTML = "La descripción no puede estar vacia"
     } else if (
-      formulario.description.value.length < 20){
+      formulario.description.value.length <= 20){
+        errores.push('descripción menor a 20 dígitos'); 
       formulario.querySelector(".errordescription").innerHTML = "La descripción debe tener al menos 20 caracteres"
     } else {
       formulario.querySelector(".errordescription").innerHTML = ""
@@ -47,7 +55,7 @@ window.addEventListener("load", function () {
     //si no fallaron las validaciones 
     if (errores.length == 0) {
       formulario.submit();
-    }
+    } else { console.log(errores)}
   }
       
   )
--- public/js/jsProductEdit.js
+++ public/js/jsProductEdit.js
@@ -4,11 +4,14 @@ window.addEventListener("load", function () {
     
     formulario.addEventListener("submit", function (event) {
     event.preventDefault();
+    let errores = [];    
               
     //validacion nombre
     if (formulario.name.value == "") {
+      errores.push('error: nombre vacío');
         formulario.querySelector(".errorname").innerHTML = "El nombre no puede estar vacío"
-      } else if (formulario.name.value.length < 2) {
+      } else if (formulario.name.value.length <= 2) {
+        errores.push('nombre menor a 2 dígitos');
         formulario.querySelector(".errorname").innerHTML = "El nombre debe tener al menos dos caracteres"
       } else {
         formulario.querySelector(".errorname").innerHTML = ""
@@ -17,8 +20,10 @@ window.addEventListener("load", function () {
     
       //validacion precio
       if (formulario.price.value == "") {
+        errores.push('precio vacío');
         formulario.querySelector(".errorprice").innerHTML = "El precio no puede estar vacío"
       } else if (formulario.price.value <= 0) {
+        errores.push('precio menor a cero');
         formulario.querySelector(".errorprice").innerHTML = "El precio debe ser mayos a cero"
       } else {
         formulario.querySelector(".errorprice").innerHTML = ""
@@ -30,11 +35,13 @@ window.addEventListener("load", function () {
       if (extension=="jpg"|| extension=="jpeg" || extension=="png" || extension=="gif"){
       formulario.querySelector(".errorimage").innerHTML = ""
      } else {
+      errores.push('error en la imagen');
       formulario.querySelector(".errorimage").innerHTML = "No se admiten archivos de extensión " + "'" + extension.toUpperCase() + "'." + " Archivos permitidos: JPG, JPEG, PNG o GIF."
       }
       
       //validacion Descripcion 
       if (formulario.description.value == "") {
+        errores.push('descripción vacía');
         formulario.querySelector(".errordescription").innerHTML = "La descripcion no puede estar vacia"
       } else {
         formulario.querySelector(".errordescription").innerHTML = ""
@@ -43,7 +50,7 @@ window.addEventListener("load", function () {
       //si no fallaron las validaciones 
       if (errores.length == 0) {
         formulario.submit();
-      }
+      } else {console.log(errores)}
     }
         
     )
