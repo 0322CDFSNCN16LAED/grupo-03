e5dab9ea635c89023b60d78f51b2c9876508c4a2
marielprado <mariel.prado24@gmail.com>
2022-09-20 16:07:36 -0300

    dashboard

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
                 {/* <!-- End of MainContent --> */}
 
                 {/* <!-- Footer --> */}
-                <footer className="sticky-footer bg-white">
+                <footer className="sticky-footer">
                     <div className="container my-auto">
                         <div className="copyright text-center my-auto">
                             <span>Copyright &copy; Dashboard 2021</span>
--- dashboard/src/components/usuarios/Usuario.jsx
+++ dashboard/src/components/usuarios/Usuario.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Usuario({usuario}) {
     return (
-        <div className="col-lg-6 mb-4">
+        <div className="col-lg-6 mb-4 w-100vw">
             <div className="card text-white bg-dark shadow"> {/*bg-dark*/}
                 <div className="card-body">
                     {usuario}                     
